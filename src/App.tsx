8abefbc4e82f5bb34e830e788e7a80d5334b755b
Yu-326-ta <yoshinaga.yuta326@icloud.com>
2023-09-11 22:11:18 +0900

    create useEffect for getting csrftoken

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,23 @@
-import React from 'react'
+import { useEffect } from 'react'
 import { Auth } from './store/components/Auth'
 import { Todo } from './store/components/Todo'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import axios from 'axios'
+import { CsrfToken } from './types'
 
 function App() {
+  useEffect(() => {
+    axios.defaults.withCredentials = true
+    const getCsrfToken = async () => {
+      // csrfのエンドポイントにアクセスしてトークン取得
+      const { data } = await axios.get<CsrfToken>(
+        `${process.env.REACT_APP_API_URL}/csrf`
+      )
+      // 取得したトークンをX-CSRF-Tokenという名前をつけてヘッダーに付与
+      axios.defaults.headers.common['X-CSRF-Token'] = data.csrf_token
+    }
+    getCsrfToken()
+  }, [])
   return (
     <BrowserRouter>
       <Routes>
--- src/index.tsx
+++ src/index.tsx
@@ -4,7 +4,7 @@ import './index.css'
 import App from './App'
 import reportWebVitals from './reportWebVitals'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools/build/lib/devtools'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 
 const queryClient = new QueryClient({})
 
