0813a432b447dd960704c27fd5948abb03347d01
Yu-326-ta <yoshinaga.yuta326@icloud.com>
2024-06-30 22:10:19 +0900

    男子キャラ画像追加

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,46 @@
+import Image from 'next/image'
+
 export default function Home() {
   return (
     <main>
-      <div>test</div>
+      <h1>Adventura</h1>
+      <h2>冒険を始めよう</h2>
+      <Image
+        src="/images/male-characters/yusha.png"
+        alt="yusha"
+        width={200}
+        height={200}
+      ></Image>
+      <Image
+        src="/images/male-characters/monk.png"
+        alt="yusha"
+        width={200}
+        height={200}
+      ></Image>
+      <Image
+        src="/images/male-characters/thief.png"
+        alt="yusha"
+        width={200}
+        height={200}
+      ></Image>
+      <Image
+        src="/images/male-characters/traveler.png"
+        alt="yusha"
+        width={200}
+        height={200}
+      ></Image>
+      <Image
+        src="/images/male-characters/wizard.png"
+        alt="yusha"
+        width={200}
+        height={200}
+      ></Image>
+      <Image
+        src="/images/male-characters/warrior.png"
+        alt="yusha"
+        width={200}
+        height={200}
+      ></Image>
     </main>
   )
 }
