aa330f3a4e191339bac36d5d76abc310a45ea18b
Yuta Yoshinaga <yoshinaga-yuta@dmm.com>
2024-09-12 01:21:33 +0900

    ログインUI作成

---
+++ src/pages/login/index.tsx
@@ -0,0 +1,15 @@
+import Header from '@/components/header/header'
+import Login from '@/components/login/login'
+import Footer from '@/components/footer/footer'
+
+export default function Home() {
+  return (
+    <>
+      <main>
+        <Header />
+        <Login />
+        <Footer />
+      </main>
+    </>
+  )
+}
