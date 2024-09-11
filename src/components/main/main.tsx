d0e1a55b8be6c854b11468d218c2f8f156f353f6
Yuta Yoshinaga <yoshinaga-yuta@dmm.com>
2024-09-12 01:22:10 +0900

    css修正

--- src/components/main/main.tsx
+++ src/components/main/main.tsx
@@ -26,13 +26,13 @@ const Main = () => {
         />
         <Link
           href="/signup"
-          className="bg-black text-white px-8 py-2 rounded-lg w-full max-w-xs border-solid border-4 border-white flex items-center justify-center"
+          className="bg-zinc-900 text-white px-8 py-2 rounded-lg w-full max-w-xs border-solid border-4 border-white flex items-center justify-center"
         >
           新規登録
         </Link>
         <Link
           href="/login"
-          className="bg-black text-white px-8 py-2 rounded-lg w-full max-w-xs border-solid border-4 border-white flex items-center justify-center"
+          className="bg-zinc-900 text-white px-8 py-2 rounded-lg w-full max-w-xs border-solid border-4 border-white flex items-center justify-center"
         >
           ログイン
         </Link>
