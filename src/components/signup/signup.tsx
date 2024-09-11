12678d90c63f4c9b7462aca3712c8512773bd5c1
Yuta Yoshinaga <yoshinaga-yuta@dmm.com>
2024-09-12 01:21:51 +0900

    新規登録UI作成

---
+++ src/components/signup/signup.tsx
@@ -0,0 +1,92 @@
+import { Sign } from 'crypto'
+import styles from './signup.module.css'
+
+const Signup = () => {
+  return (
+    <>
+      <section>
+        <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
+          <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
+            <div className="p-6 space-y-4 md:space-y-6 sm:p-8 bg-zinc-900 text-white border-solid border-4 border-white rounded-lg">
+              <h1 className="text-xl leading-tight tracking-tight md:text-2xl dark:text-white">
+                新規登録
+              </h1>
+              <form className="space-y-4 md:space-y-6">
+                <div>
+                  <label
+                    htmlFor="name"
+                    className="block mb-2 text-sm font-medium dark:text-white"
+                  >
+                    ニックネーム
+                  </label>
+                  <input
+                    type="name"
+                    name="name"
+                    id="name"
+                    className="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                    placeholder="name"
+                    //   value={email}
+                    //   onChange={(e) => setEmail(e.target.value)}
+                    required
+                  />
+                </div>
+                <div>
+                  <label
+                    htmlFor="email"
+                    className="block mb-2 text-sm font-medium dark:text-white"
+                  >
+                    メールアドレス
+                  </label>
+                  <input
+                    type="email"
+                    name="email"
+                    id="email"
+                    className="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                    placeholder="name@company.com"
+                    //   value={email}
+                    //   onChange={(e) => setEmail(e.target.value)}
+                    required
+                  />
+                </div>
+                <div>
+                  <label
+                    htmlFor="password"
+                    className="block mb-2 text-sm font-medium dark:text-white"
+                  >
+                    パスワード
+                  </label>
+                  <input
+                    type="password"
+                    name="password"
+                    id="password"
+                    className="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                    placeholder="••••••••"
+                    //   value={password}
+                    //   onChange={(e) => setPassword(e.target.value)}
+                    required
+                  />
+                </div>
+                <button
+                  type="submit"
+                  className="w-full text-white focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-lg px-5 py-2.5 text-center"
+                >
+                  アカウント作成
+                </button>
+                <p className="text-sm dark:text-gray-400 flex justify-center items-center">
+                  <a
+                    href="/login"
+                    className="font-medium hover:underline dark:text-primary-500"
+                  >
+                    ログインはこちら
+                  </a>
+                </p>
+              </form>
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
+  )
+}
+
+export default Signup
---
+++ src/pages/signup/index.tsx
@@ -0,0 +1,15 @@
+import Header from '@/components/header/header'
+import Signup from '@/components/signup/signup'
+import Footer from '@/components/footer/footer'
+
+export default function Home() {
+  return (
+    <>
+      <main>
+        <Header />
+        <Signup />
+        <Footer />
+      </main>
+    </>
+  )
+}
