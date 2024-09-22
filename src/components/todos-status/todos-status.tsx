6f10e79def0be1c6b6c809081c9e1bdb2b5d29b6
Yuta Yoshinaga <yoshinaga-yuta@dmm.com>
2024-09-23 04:03:57 +0900

    todo一覧のステータス作成

---
+++ src/components/todos-status/todos-status.tsx
@@ -0,0 +1,32 @@
+import Image from 'next/image'
+import styles from './main.module.css'
+import Link from 'next/link'
+
+const TodosStatus = () => {
+  return (
+    <div className="flex flex-col md:flex-row items-center justify-center md:py-16 md:px-64 pt-12">
+      <div className="flex flex-col items-center md:mx-8 min-w-[200px] min-h-[200px]">
+        <Image
+          src="/images/male-characters/m-warrior.png"
+          alt="king"
+          width={200}
+          height={200}
+        />
+      </div>
+      <div className="rpg-blowing py-4 px-12 md:mx-8 md:mt-0 mt-8 min-w-[400px]">
+        <p className="text-lg text-left text-white text-xl">
+          ゆうた
+          <br />
+          職業：せんし
+          <br />
+          Lv：50
+          <br />
+          次のレベル
+        </p>
+        <progress className="w-full mt-2" value="70" max="100"></progress>
+      </div>
+    </div>
+  )
+}
+
+export default TodosStatus
--- src/pages/todos/index.tsx
+++ src/pages/todos/index.tsx
@@ -1,13 +1,14 @@
 import Header from '@/components/header-after-login/header-after-login'
 import Login from '@/components/login/login'
 import Footer from '@/components/footer/footer'
+import TodosStatus from '@/components/todos-status/todos-status'
 
 export default function Home() {
   return (
     <>
       <main className="home-bg">
         <Header />
-        <h1>Todos</h1>
+        <TodosStatus />
         <Footer />
       </main>
     </>
